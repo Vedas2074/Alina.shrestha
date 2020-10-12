6abebf5c1adb2f255ec4d1824f35baf75190fea8
Ali-nah <alinashrestha723@gmail.com>
2020-10-12 23:35:11 +0545

    LearnInheritance

---
+++ CSharpbasics/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using LearningClasses;
+namespace CSharpbasics
+{
+    class Program
+    {
+        static void Main() 
+        {
+            var person2 = new Person();
+            var person1 = new Person("d", "Kathmandu", 20);  
+            Console.WriteLine(person1.Address); 
+
+            var s = Calcalutor.Sum<int>(2,3);
+
+            Rectangle rectangle = new Rectangle(2.3,4.5);
+            
+            var result = rectangle.GetArea();
+            
+        }
+    }
+}
+
+
